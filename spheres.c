f7eb0f9289020f0a9f597ba07ce0d7a88c8dfe00
Abel Pampin <qayinhabel@gmail.com>
2017-12-24 16:20:11 +0100

    violacion de core

--- spheres.c
+++ spheres.c
@@ -8,33 +8,33 @@
 #include <errno.h>
 
 //	CONSTANTs
-define FILE_MAX 255;
+#define FILE_MAX 255
 
 //	FUNCTION DECLARATION
-
-
-
+void openFile(FILE *fp);
 
 //	M A I N
 int main(void){
 
-	char filename[FILE_MAX];	//strings
-	FILE *file_pointer;		//FILE POINTER
-
-	do{
-		fprintf(stdout,"Enter name of DATAFILE:\t");
-		fscanf(stdin,"%s",filename);
-		file_pointer = fopen(filename,"r");
-		if (file_pointer == NULL){
-			fprintf(stderr,"Error, file not open.\n");
-			perror("Description:");
-		}
-
-	}while(file_pointer == NULL);
-
+		//VARS
+	FILE *file_pointer = NULL;	//FILE POINTER
 	
 
+	openFile(file_pointer);
 	puts("FILE OPENED!");
 	fclose(file_pointer);
 }
 
+void openFile(FILE *fp){
+
+	char filename[FILE_MAX];	//string	
+	do{
+		fprintf(stdout,"Enter name of DATAFILE:\t");
+		fscanf(stdin,"%s",filename);
+		fp = fopen(filename,"r");
+		if (fp == NULL){
+			fprintf(stderr,"Error, file not open.\n");
+			perror("Description:");
+		}
+	}while(fp == NULL);
+}
