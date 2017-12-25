4b8c19b6c2841ed11f6d9ddd01a11c1523db5960
Abel Pampin <qayinhabel@gmail.com>
2017-12-25 20:46:27 +0100

    nuevas funciones. sin compilar

--- spheres.c
+++ spheres.c
@@ -10,6 +10,8 @@
 //	CONSTANTs
 #define FILE_MAX 255
 #define MAX 100			//max 100 spheres
+#define PI 3.14
+#define VOL_LIM 215.0
 
 //	STRUCTS
 struct sphere{
@@ -20,6 +22,11 @@ struct sphere{
 //	FUNCTION DECLARATION
 FILE* openFile(void);
 int readFile(FILE* fp,struct sphere *arr_sphere);
+//void change_rad(struct sphere *esfera,double new_rad);
+//void change_clr(struct sphere *esfera,char *new_clr);
+void print_report(struct sphere *arr_sphere,int num_spheres);
+double calculate_area(struct sphere esfera);
+double calculate_volume(struct sphere *esfera); //just to see the diff
 
 
 //	M A I N
@@ -36,15 +43,7 @@ int main(void){
 	
 	
 
-	for(int i = 0;i < num_spheres;i++){	//verificacion
-		
-		double data;
-		char color;
-	
-		color = sphere_data[i].color;
-		data = sphere_data[i].radio;
-		printf("Esfera n%d:\t%lf\t%c\n",i,data,color);
-	}
+
 	
 }
 
@@ -109,6 +108,90 @@ int readFile(FILE* fp,struct sphere *arr_sphere){
 
 	return i;
 }
+
+
+double calculate_area(struct sphere esfera){
+	//if I include math (link) I could use a better aproximation of PI.
+		
+	//vars	
+	double area,radio;
+
+	radio = esfera.radio;
+	area  = 4*PI*radio*radio;
+
+	return area;
+
+}
+
+double calculate_volume(struct sphere *esfera){
+	//same process as before but I used pointer instead
+
+	//vars
+	double volume,radio;
+	
+	radio  = (esfera->radio);
+	volume = (4.0/3)*PI*radio*radio*radio;	//homemade cast to get 4/3 right	
+	//Could I use pow from math.h?
+
+	return volume;
+}
+
+void print_report(struct sphere *arr_sphere,int num_spheres){
+	//generates updated report of the sphere data as ruled in rules.txt
+	
+	//VARs
+	int num_color;
+	int num_red = 0;
+	int num_vol = 0;
+	double rad,area,vol;
+	double total_area = 0.0;
+	char color;
+	char *colores[] = {"Blue","Yellow","Red","Pink","NoColor"};
+
+	fprintf(stdout,"\n\tNumber\t\tRadius\t\tColor\t\tArea\t\tVolume\n");
+	for(int i = 0;i < num_spheres;i++){
+		
+		//getters
+		color  = arr_sphere[i]->color;
+		rad    = arr_sphere[i]->radio;			//SEE FUNCTION DECLARATION
+		area   = calculate_area(arr_sphere[i]);		//content of sphere[i]
+		volume = calculate_volume(&arr_sphere[i]);	//address of sphere[i]  
+		//info
+		total_area += area;
+		if (volume > VOL_LIM) num_vol++;
+	
+		switch (color){
+			case 'b':
+				num_color = 0;
+				break;
+			case 'y':
+				num_color = 1;
+				break;
+			case 'r':
+				num_color = 2;
+				num_red++;
+				break;
+			case 'p':
+				num_color = 3;
+				break;
+			default:
+				num_color = 4;
+		}
+		
+		fprintf(stdout,"\t%d\t\t%.2lf\t\t%s\t\t%.2lf\t\t%.2lf\n"
+		,(i+1),rad,colores[num_color],area,vol);
+	}
+	
+	//Extra data: num of spheres, %red, num Vol>215 and average area.
+
+	fprintf(stdout,"Total number of spheres:\t%d",num_spheres);
+	fprintf(stdout,"%.2lf % of spheres are red.\n",(double)num_red/num_spheres);
+	fprintf(stdout,"%d spheres have a volume over the %.2lf limit.\n",num_vol,VOL_LIM);
+	fprintf(stdout,"Average area:\t%.2lf\n",total_area/num_spheres);
+
+}
+
+
 
 /*	PROBLEMS:
  *WHY DO IT READS ONE/TWO UNEXISTANT SPHERES AT THE END?
